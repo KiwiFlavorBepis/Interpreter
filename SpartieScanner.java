8391cf2f797a9ff47028d40025b08dc30294201d
Kieran Williams <kww26@case.edu>
2024-10-22 20:14:52 -0400

    Finished getSingleCharacterToken

--- SpartieScanner.java
+++ SpartieScanner.java
@@ -59,14 +59,68 @@ public class SpartieScanner {
 
     // TODO: Complete implementation
     private Token getSingleCharacterToken() {
+        /* Single character tokens should include:
+           Simple tokens
+            SEMICOLON,
+            COMMA,
+            ASSIGN, // =
+            LEFT_BRACE, RIGHT_BRACE, // { }
+            LEFT_PAREN, RIGHT_PAREN, // ( )
+            MULTIPLY, //  *
+            ADD, SUBTRACT, // + -
+            NOT, // !
+           Logical
+            AND, OR, // & |
+         */
+
         // Hint: Examine the character, if you can get a token, return it, otherwise return null
         // Hint: Be careful with the divide, we have ot know if it is a single character
-
-        char nextCharacter = source.charAt(current);
-
         // Hint: Start of not knowing what the token is, if we can determine it, return it, otherwise, return null
         TokenType type = TokenType.UNDEFINED;
+        char nextCharacter = source.charAt(current);
+        switch (nextCharacter) {
+            case ';':
+                type = TokenType.SEMICOLON;
+                break;
+            case ',':
+                type = TokenType.COMMA;
+                break;
+            case '=':
+                type = TokenType.ASSIGN;
+                break;
+            case '{':
+                type = TokenType.LEFT_BRACE;
+                break;
+            case '}':
+                type = TokenType.RIGHT_BRACE;
+                break;
+            case '(':
+                type = TokenType.LEFT_PAREN;
+                break;
+            case ')':
+                type = TokenType.RIGHT_PAREN;
+                break;
+            case '*':
+                type = TokenType.MULTIPLY;
+                break;
+            case '+':
+                type = TokenType.ADD;
+                break;
+            case '-':
+                type = TokenType.SUBTRACT;
+                break;
+            case '!':
+                type = TokenType.NOT;
+                break;
+            case '&':
+                type = TokenType.AND;
+                break;
+            case '|':
+                type = TokenType.OR;
+                break;
+        }
 
+        if (type != TokenType.UNDEFINED) return new Token(type, String.valueOf(nextCharacter), line);
         return null;
     }
 
@@ -117,18 +171,14 @@ public class SpartieScanner {
     }
 
     private boolean isAlpha(char character) {
-        return character >= 'a' && character <= 'z' ||
-                character >= 'A' && character <= 'Z';
+        return character >= 'a' && character <= 'z' || character >= 'A' && character <= 'Z';
     }
 
     // This will check if a character is what you expect, if so, it will advance
     // Useful for checking <= or //
     private boolean examine(char expected) {
         if (isAtEnd()) return false;
-        if (source.charAt(current + 1) != expected) return false;
-
-        // Otherwise, it matches it, so advance
-        return true;
+        return source.charAt(current + 1) == expected;
     }
 
     private boolean isAtEnd() {
